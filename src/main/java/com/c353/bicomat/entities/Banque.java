237fd928e3738e100859e3bd0bb0d895a9c0c277
DINAH <aladji.dinah@gmail.com>
2018-01-05 13:57:22 +0000

    Compte Bancaire

--- src/main/java/com/c353/bicomat/entities/Banque.java
+++ src/main/java/com/c353/bicomat/entities/Banque.java
@@ -1,9 +1,13 @@
 package com.c353.bicomat.entities;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 
 /**
  * 
@@ -16,44 +20,71 @@ public class Banque {
 	
 	@javax.persistence.Id
 	@GeneratedValue(strategy=GenerationType.SEQUENCE)
-	private Long Id;
+	private Long id;
 	
 	@Column
-	private String Nom;
+	private String nom;
 	
 	@Column
-	private String Adresse;
+	private String adresse;
+
+	 @OneToMany
+	 @JoinColumn( name = "id" )
+	 private List<CompteBancaire>  comptebancaires;
+	    
 
 	public Long getId() {
-		return Id;
+		return id;
 	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 
 	public String getNom() {
-		return Nom;
+		return nom;
 	}
+
 
 	public void setNom(String nom) {
-		Nom = nom;
+		this.nom = nom;
 	}
+
 
 	public String getAdresse() {
-		return Adresse;
+		return adresse;
 	}
 
+
 	public void setAdresse(String adresse) {
-		Adresse = adresse;
+		this.adresse = adresse;
 	}
+
+	
+
+	public List<CompteBancaire> getComptebancaires() {
+		return comptebancaires;
+	}
+
+
+	public void setComptebancaires(List<CompteBancaire> comptebancaires) {
+		this.comptebancaires = comptebancaires;
+	}
+
 
 	public Banque(Long id, String nom, String adresse) {
 		super();
-		Id = id;
-		Nom = nom;
-		Adresse = adresse;
+		this.id = id;
+		this.nom = nom;
+		this.adresse = adresse;
 	}
+
 
 	public Banque() {
 		super();
-		// TODO Auto-generated constructor stub
+		
 	}
 	
 	
