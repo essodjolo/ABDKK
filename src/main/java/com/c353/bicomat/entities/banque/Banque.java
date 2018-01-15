948afade9ac851e86b02938cf29ce8ff2c085555
bi-ayefo <marc.atsou@gmail.com>
2018-01-15 02:11:53 +0100

    Commentaire entité Banque.

--- src/main/java/com/c353/bicomat/entities/banque/Banque.java
+++ src/main/java/com/c353/bicomat/entities/banque/Banque.java
@@ -5,74 +5,59 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
-
 /**
- * 
+ * Classe entité pour Banque.
+ *
  * @author DINAH Aladji Jérémie
  *
  */
-
 @Entity
 public class Banque {
-	
-	@javax.persistence.Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	private Integer id;
-	
-	@Column
-	private String nom;
-	
-	@Column
-	private String adresse;
 
-	
+    @javax.persistence.Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
 
-	public Integer getId() {
-		return id;
-	}
+    @Column
+    private String nom;
 
+    @Column
+    private String adresse;
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public String getNom() {
-		return nom;
-	}
+    public String getNom() {
+        return nom;
+    }
 
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
 
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
+    public String getAdresse() {
+        return adresse;
+    }
 
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
 
-	public String getAdresse() {
-		return adresse;
-	}
+    public Banque(Integer id, String nom, String adresse) {
+        super();
+        this.id = id;
+        this.nom = nom;
+        this.adresse = adresse;
+    }
 
+    public Banque() {
+        super();
 
-	public void setAdresse(String adresse) {
-		this.adresse = adresse;
-	}
-
-	
-
-
-	public Banque(Integer id, String nom, String adresse) {
-		super();
-		this.id = id;
-		this.nom = nom;
-		this.adresse = adresse;
-	}
-
-
-	public Banque() {
-		super();
-		
-	}
-	
-	
-	
+    }
 
 }
